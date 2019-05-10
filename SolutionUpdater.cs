f879f3b4bd58c82f1ab884c298a68a4f089f8e4d
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@devexpress.com>
2019-05-10 19:27:23 +0300

    Add project files.

---
+++ SolutionUpdater.cs
@@ -0,0 +1,140 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using System.IO;
+using System.Xml;
+
+namespace ProjectUpdater
+{
+    public class SolutionUpdater
+    {
+        string sItemGroup = "ItemGroup";
+        string sReference = "Reference";
+        string sInclude = "Include";
+        string sDevExpress = "DevExpress";
+        string sLicx = "licx";
+        string sResx = "resx";
+        string sProperties = @"\Properties";
+        string sMyProject = @"\My Project";
+        string sEmbeddedResource = "EmbeddedResource";
+        string sAssembly = "assembly";
+        string sName = "name";
+
+        public void UpdateSolution(DTE2 dte)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+            if (dte != null && dte.Solution != null)
+                foreach (Project proj in dte.Solution.Projects)
+                {
+                    string projFileName = proj.FileName;
+                    if (string.IsNullOrWhiteSpace(projFileName))
+                        continue;
+                    DeleteLicx(projFileName);
+                    UpdateProj(projFileName);
+                }
+        }
+
+        void DeleteLicx(string projFileName)
+        {
+            string propertiesFolder = GetParentDirectory(projFileName);
+            propertiesFolder += Directory.Exists(propertiesFolder + sProperties) ? sProperties : sMyProject;
+            string[] allFiles = Directory.GetFiles(propertiesFolder, string.Format("*.{0}", sLicx));
+            foreach (string file in allFiles)
+                File.Delete(file);
+        }
+
+        void UpdateProj(string projFileName)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(projFileName);
+
+            if (PerformUpdate(doc, true) | PerformUpdate(doc, false, GetParentDirectory(projFileName)))
+                doc.Save(projFileName);
+        }
+
+        private bool PerformUpdate(XmlDocument doc, bool references, string docParentDirectory = "")
+        {
+            string tagName = references ? sReference : sEmbeddedResource;
+            XmlNodeList projNodes = doc.GetElementsByTagName(tagName);
+            string nodeValue;
+            string newNodeValue;
+            XmlNode node;
+            bool res = false;
+            for (int nodeIndex = 0; nodeIndex < projNodes.Count; nodeIndex++)
+            {
+                node = projNodes[nodeIndex];
+                if (node.ParentNode.Name == sItemGroup)
+                    for (int attributeIndex = 0; attributeIndex < node.Attributes.Count; attributeIndex++)
+                    {
+                        nodeValue = node.Attributes[attributeIndex].Value;
+                        if (node.Attributes[attributeIndex].Name == sInclude)
+                            if (references)
+                            {
+                                newNodeValue = RemoveReferenceVersion(nodeValue);
+                                if (nodeValue != newNodeValue)
+                                {
+                                    node.Attributes[attributeIndex].Value = newNodeValue;
+                                    res = true;
+                                }
+                            }
+                            else
+                            {
+                                if (nodeValue.Contains(sLicx))
+                                {
+                                    node.ParentNode.RemoveChild(node);
+                                    res = true;
+                                }
+                                else if (nodeValue.Contains(sResx))
+                                    ProcessResx(docParentDirectory + string.Format(@"\{0}", nodeValue));
+                            }
+                    }
+            }
+            return res;
+        }
+
+        void ProcessResx(string filePath)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+            XmlNodeList projNodes = doc.GetElementsByTagName(sAssembly);
+            string nodeValue;
+            string newNodeValue;
+            XmlNode node;
+            bool res = false;
+            for (int nodeIndex = 0; nodeIndex < projNodes.Count; nodeIndex++)
+            {
+                node = projNodes[nodeIndex];
+                for (int attributeIndex = 0; attributeIndex < node.Attributes.Count; attributeIndex++)
+                {
+                    nodeValue = node.Attributes[attributeIndex].Value;
+                    if (node.Attributes[attributeIndex].Name == sName)
+                    {
+                        newNodeValue = RemoveReferenceVersion(nodeValue);
+                        if (nodeValue != newNodeValue)
+                        {
+                            node.Attributes[attributeIndex].Value = newNodeValue;
+                            res = true;
+                        }
+                    }
+                }
+            }
+            if (res)
+                doc.Save(filePath);
+        }
+
+        private string RemoveReferenceVersion(string str)
+        {
+            if (str.Contains(sDevExpress))
+            {
+                int startIndex = str.IndexOf(',');
+                if (startIndex >= 0)
+                    str = str.Remove(startIndex);
+            }
+            return str;
+        }
+
+        private string GetParentDirectory(string projFileName)
+        {
+            return Directory.GetParent(projFileName).FullName;
+        }
+    }
+}
---
+++ UpdateReferencesCommand.cs
@@ -0,0 +1,99 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="UpdateReferences.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+using EnvDTE;
+using EnvDTE80;
+
+namespace ProjectUpdater
+{
+#region DefaultCode
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class UpdateReferencesCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("5ca16a3a-1242-4918-914d-68fe0d0ff525");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateReferencesCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private UpdateReferencesCommand(Package package)
+        {
+            if (package == null)
+                throw new ArgumentNullException("package");
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static UpdateReferencesCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new UpdateReferencesCommand(package);
+        }
+        #endregion
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            new SolutionUpdater().UpdateSolution(GetDte());
+        }
+
+        private DTE2 GetDte()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            return (DTE2)(this.ServiceProvider.GetService(typeof(DTE)) as DTE);
+        }
+    }
+}
